7fee7b73290cb0d0ce7967e6b489d95f2835d7cd
w2fong <w2fong@yahoo.com>
2020-04-16 18:45:47 -0700

    Update getUpstreamBuild.groovy

--- vars/getUpstreamBuild.groovy
+++ vars/getUpstreamBuild.groovy
@@ -1,5 +1,5 @@
-def call(cred) {
+def call(userToken) {
     echo "Getting Upstream Build"
-    sh 'curl -X GET -u ${cred} http://usajenkins01.altium.biz:8080/me/my-views/view/all/job/DevOps/job/test-UAT-deployment-collector/11/api/json'
+    sh "curl -X GET -u ${userToken} http://usajenkins01.altium.biz:8080/me/my-views/view/all/job/DevOps/job/test-UAT-deployment-collector/11/api/json"
 }
 
