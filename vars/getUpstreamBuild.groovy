1762906c286c842c1f902b96a082c7b71911c7c4
w2fong <w2fong@yahoo.com>
2020-04-17 15:23:02 -0700

    Update getUpstreamBuild.groovy

--- vars/getUpstreamBuild.groovy
+++ vars/getUpstreamBuild.groovy
@@ -1,6 +1,7 @@
 def call(String userToken, String fileName ) {
     echo "Getting Upstream Build"
-    sh "curl -s -X GET -u ${userToken} http://usajenkins01.altium.biz:8080/job/DevOps/job/test-UAT-deployment-collector/${env.BUILD_NUMBER}/api/json | jq '.actions[0].causes[0].upstreamProject' > ${fileName} "
+    //sh "curl -s -X GET -u ${userToken} http://usajenkins01.altium.biz:8080/job/DevOps/job/test-UAT-deployment-collector/${env.BUILD_NUMBER}/api/json | jq '.actions[0].causes[0].upstreamProject' > ${fileName} "
+    sh "curl -s -X GET -u ${userToken} http://usajenkins01.altium.biz:8080/job/DevOps/job/test-UAT-deployment-collector/${env.BUILD_NUMBER}/api/json > ${fileName} "
 }
 
 //     | jq '.actions[0].causes[0].upstreamProject'
@@ -11,3 +12,7 @@ def setUpstreamJobInDesc(String fileName) {
     currentBuild.displayName = upstreamBuild
 }
 
+def testJson(String fileName) {
+    def jsonObject = new groovy.json.JsonSlurperClassic().parseText(readFile("${fileName}"))
+    currentBuild.displayName = jsonObject.actions[0].causes[0].upstreamProject
+}
