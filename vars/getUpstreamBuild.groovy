c44ff3f92a7c1bb83978275101077ec52b5a950e
w2fong <w2fong@yahoo.com>
2020-04-16 18:16:08 -0700

    Update getUpstreamBuild.groovy

--- vars/getUpstreamBuild.groovy
+++ vars/getUpstreamBuild.groovy
@@ -1,6 +1,5 @@
 def call() {
     echo "Getting Upstream Build"
-    def upstream = currentBuild.rawBuild.getCause(hudson.model.Cause$UpstreamCause)
-    echo upstream?.shortDescription
+    sh 'curl -x GET http://usajenkins01.altium.biz:8080/me/my-views/view/all/job/DevOps/job/test-UAT-deployment-collector/11/api/json'
 }
 
