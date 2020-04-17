9c568399f114623a62b9dd7e18d2bd858b411bc0
w2fong <w2fong@yahoo.com>
2020-04-17 15:13:35 -0700

    Update getUpstreamBuild.groovy

--- vars/getUpstreamBuild.groovy
+++ vars/getUpstreamBuild.groovy
@@ -1,6 +1,13 @@
 def call(String userToken, String fileName ) {
     echo "Getting Upstream Build"
-    sh "curl -s -X GET -u ${userToken} http://usajenkins01.altium.biz:8080/job/DevOps/job/test-UAT-deployment-collector/${env.BUILD_NUMBER}/api/json > ${fileName} "
+    sh "curl -s -X GET -u ${userToken} http://usajenkins01.altium.biz:8080/job/DevOps/job/test-UAT-deployment-collector/${env.BUILD_NUMBER}/api/json | jq '.actions[0].causes[0].upstreamProject' > ${fileName} "
 }
 
+//     | jq '.actions[0].causes[0].upstreamProject'
+
+def setUpstreamJobInDesc(String fileName) {
+    def upstreamBuild = readFile("${fileName}")
+    echo upstreamBuild
+    currentBuild.displayName = upstreamBuild
+}
 
