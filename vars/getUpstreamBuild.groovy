3eb1266df4dcb4133cf6bb87ceab053b7325a9b9
w2fong <w2fong@yahoo.com>
2020-04-17 17:00:08 -0700

    Update getUpstreamBuild.groovy

--- vars/getUpstreamBuild.groovy
+++ vars/getUpstreamBuild.groovy
@@ -14,6 +14,6 @@ def setUpstreamJobInDesc(String fileName) {
 
 def testJson(String fileName) {
     def upstreamProject = sh "jq '.actions[0].causes[0].upstreamProject' ${fileName}"
-    print(upstreamBuild)
+    print(upstreamProject)
     currentBuild.displayName = upstreamProject
 }
