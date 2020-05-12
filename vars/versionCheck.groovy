effc291aefe28a463a38b6c1f651b04c790b20cc
w2fong <w2fong@yahoo.com>
2020-05-11 19:26:11 -0700

    Update versionCheck.groovy

--- vars/versionCheck.groovy
+++ vars/versionCheck.groovy
@@ -1,7 +1,7 @@
 def call(String appVersion, String appName ) {
     echo "Getting Application version ${appName}"
     if (${appName} == "ExternalHelperService" ) {
-      if (${appVersion} == 5.27.0.2 ) {
+      if (${appVersion} == "5.27.0.2" ) {
         return false
       }
       else {
