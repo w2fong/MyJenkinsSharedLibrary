7e5f88e976c211fa1dce77187c992175e50beaa0
w2fong <w2fong@yahoo.com>
2020-05-11 19:42:29 -0700

    Update versionCheck.groovy

--- vars/versionCheck.groovy
+++ vars/versionCheck.groovy
@@ -1,16 +1,41 @@
 def call(String appVersion, String appName ) {
     echo "Getting Application version ${appName}"
     script {
-        if ("${appName}" == "ExternalHelperService" ) {
-          if ("${appVersion}" == "5.27.0.2" ) {
-            return false
-          }
-          else {
-            return true
-          }
+        switch("${appName}") { 
+            case "ExternalHelperService": 
+                if ("${appVersion}" == "5.27.0.2" ) {
+                    return false
+                }
+                else {
+                    return true
+                }
+            case "InternalHelperService": 
+                if ("${appVersion}" == "5.27.0.1" ) {
+                    return false
+                }
+                else {
+                    return true
+                }
+            case "TES": 
+                if ("${appVersion}" == "5.27.0.1" ) {
+                    return false
+                }
+                else {
+                    return true
+                }
+            case "CCV": 
+                if ("${appVersion}" == "5.27.0.4" ) {
+                    return false
+                }
+                else {
+                    return true
+                }
+            case "KernelStateless":
+                return true
+            case "KernelStateful":
+                return true
+            default:
+                statement #Default 
         }
-        else {
-          return true
-        }
-    }        
+    }
 }
